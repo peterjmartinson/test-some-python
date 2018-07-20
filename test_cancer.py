33f1c425e9b639755ea1388051ab563b5ab47efb
Peter <peter.j.martinson@gmail.com>
2018-07-19 21:29:33 -0400

    Make some tests

--- test_cancer.py
+++ test_cancer.py
@@ -1,19 +1,5 @@
 from BreastCancerML import *
 
-# import numpy as np
-# import pandas as pd
-
-# from sklearn.metrics import classification_report
-# from sklearn.metrics import confusion_matrix
-# from sklearn.metrics import accuracy_score
-# from sklearn.model_selection import train_test_split
-# from sklearn.model_selection import cross_val_score
-# from sklearn.model_selection import KFold
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.svm import SVC
-# from sklearn.linear_model import LogisticRegression
-# import time
-
 def test_read_csv():
     data = pd.read_csv('Data/data.csv', index_col=False)
     assert data.empty == False
@@ -37,27 +23,6 @@ def test_pumper_NotCsv():
     assert result == 0
 
 
-
-
-
-
-
-# v = UnixModemConfigurator()
-# h = HayesModem()
-# z = ZoomModem()
-# e = ErnieModem()
-
-# def test_HayesForUnix():
-#   h.Accept(v);
-#   assert  "&s1=4&D=3" == h.configurationString
-
-# def test_ZoomForUnix():
-#   z.Accept(v);
-#   assert  42 == z.configurationValue
-
-# def test_HayesForUnix():
-#   e.Accept(v);
-#   assert  "C is too slow" == e.internalPattern
 
 
 
