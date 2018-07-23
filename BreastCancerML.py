782985ba9b0e221ef6726feb52542880a6115a31
Peter Martinson <peter.j.martinson@gmail.com>
2018-07-22 22:50:45 -0400

    Put the model into the class

--- BreastCancerML.py
+++ BreastCancerML.py
@@ -14,30 +14,47 @@ import time
 
 class BreastCancerPredictionMachine(object):
 
-  def getData(self, input_data):
-    if input_data == None:
-      print("Please enter data")
-      return 0
-    elif input_data[-4:] != ".csv":
-      return 0
-    else:
-      return 1
+    def setInput(self, input_data):
+        if len(input_data) != 31:
+            raise ValueError('input must be a list with 31 elements')
+        numpy_array = np.array(input_data)
+        return numpy_array
 
-  def setInput(self, input_data):
-      return 1
+    def getDiagnosis(self, data):
+        if type(data) is not list:
+            raise TypeError('input must be a list')
+        # Train the model
+        training_data = 'Data/data.csv'
+        pandas_data = pd.read_csv(training_data, index_col=False)
+        Y = pandas_data['diagnosis'].values # Get everything down the diagnosis column
+        X = pandas_data.drop('diagnosis', axis=1).values # get everything *but* the diagnosis column
+        scaler = StandardScaler().fit(X)
+        X_scaled = scaler.transform(X) ## normalize the X values (2d matrix)
+        model = SVC(C=2.0, kernel='rbf') ## choose the model
+        model.fit(X_scaled, Y) ## train the model.  (input values, answers)
+        # Make the prediction
+        sample_data = self.setInput(data)
+        sample_data_scaled = scaler.transform(sample_data.reshape(1,-1)) ## One line of values, normalized.  These are the test values
+        predictions = model.predict(sample_data_scaled) ## Output - what should these values give you?
+        return predictions[0]
+        # return 'Mn'
+
+
   
 
 # bring in the data with Pandas and print the first few lines
-input_data = 'Data/data.csv'
-data = pd.read_csv(input_data, index_col=False)
-print(data.head(5))
+
+# input_data = 'Data/data.csv'
+# data = pd.read_csv(input_data, index_col=False)
+# print(data.head(5))
 
 # assign data to the correct axes
-Y = data['diagnosis'].values # Get everything down the diagnosis column
-X = data.drop('diagnosis', axis=1).values # get everything *but* the diagnosis column
+
+# Y = data['diagnosis'].values # Get everything down the diagnosis column
+# X = data.drop('diagnosis', axis=1).values # get everything *but* the diagnosis column
 
 
-test_set = np.array([0.1425, 0.2839, 0.2414, 0.1052, 0.2597, 0.09744, 0.4956, 0.00911, 0.07458, 0.05661, 0.01867, 0.05963, 0.009208, 0.2098, 0.8663, 0.6869, 0.2575, 0.6638, 0.173, 1.156, 3.445, 11.42, 14.91, 20.38, 26.5, 27.23, 77.58, 98.87, 386.1, 567.7, 84348301])
+# test_set = np.array([0.1425, 0.2839, 0.2414, 0.1052, 0.2597, 0.09744, 0.4956, 0.00911, 0.07458, 0.05661, 0.01867, 0.05963, 0.009208, 0.2098, 0.8663, 0.6869, 0.2575, 0.6638, 0.173, 1.156, 3.445, 11.42, 14.91, 20.38, 26.5, 27.23, 77.58, 98.87, 386.1, 567.7, 84348301])
 
 
 
@@ -46,37 +63,37 @@ test_set = np.array([0.1425, 0.2839, 0.2414, 0.1052, 0.2597, 0.09744, 0.4956, 0.
 # --------------------------------------------------------------------------------
 ## The below "is not needed"
 # Below shows SVC is better than Logistics model; DO NOT NEED FOR PRODUCTION!
-num_folds = 10
-kfold = KFold(n_splits=num_folds, random_state=123)
-start = time.time()
-cv_results = cross_val_score(LogisticRegression(), X, Y, cv=kfold, scoring='accuracy')
-end = time.time()
-print( "Logistics regression accuracy: %f, run time: %f)" % (cv_results.mean(), end-start))
+# num_folds = 10
+# kfold = KFold(n_splits=num_folds, random_state=123)
+# start = time.time()
+# cv_results = cross_val_score(LogisticRegression(), X, Y, cv=kfold, scoring='accuracy')
+# end = time.time()
+# print( "Logistics regression accuracy: %f, run time: %f)" % (cv_results.mean(), end-start))
 
-start = time.time()
-scaler = StandardScaler().fit(X)
-X_scaled = scaler.transform(X)
-cv_results = cross_val_score(SVC(C=2.0, kernel="rbf"), X_scaled, Y, cv=kfold, scoring='accuracy')
-end = time.time()
-print( "SVC accuracy: %f, run time: %f)" % (cv_results.mean(), end-start))
+# start = time.time()
+# scaler = StandardScaler().fit(X)
+# X_scaled = scaler.transform(X)
+# cv_results = cross_val_score(SVC(C=2.0, kernel="rbf"), X_scaled, Y, cv=kfold, scoring='accuracy')
+# end = time.time()
+# print( "SVC accuracy: %f, run time: %f)" % (cv_results.mean(), end-start))
 ## The above "is not needed"
 # --------------------------------------------------------------------------------
 
 ## The below is the "training"
 # THIS IS WHAT IS NEEDED FOR PRODUCTION TO ESTIMATE THE MODEL
-scaler = StandardScaler().fit(X)
-X_scaled = scaler.transform(X) ## normalize the X values (2d matrix)
-model = SVC(C=2.0, kernel='rbf') ## choose the model
-model.fit(X_scaled, Y) ## train the model.  (input values, answers)
+# scaler = StandardScaler().fit(X)
+# X_scaled = scaler.transform(X) ## normalize the X values (2d matrix)
+# model = SVC(C=2.0, kernel='rbf') ## choose the model
+# model.fit(X_scaled, Y) ## train the model.  (input values, answers)
 
 
 # THIS IS WHAT IS NEEDED FOR PREDICTION
-X_test_scaled = scaler.transform(X[10,:].reshape(1,-1)) ## One line of values, normalized.  These are the test values
-predictions = model.predict(X_test_scaled) ## Output - what should these values give you?
+# X_test_scaled = scaler.transform(X[10,:].reshape(1,-1)) ## One line of values, normalized.  These are the test values
+# predictions = model.predict(X_test_scaled) ## Output - what should these values give you?
 
-print("Predicted: {}, actual: {}".format(predictions[0], Y[10]));
+# print("Predicted: {}, actual: {}".format(predictions[0], Y[10]));
 
-X_test_scaled = scaler.transform(test_set.reshape(1,-1)) ## One line of values, normalized.  These are the test values
-predictions = model.predict(X_test_scaled) ## Output - what should these values give you?
+# X_test_scaled = scaler.transform(test_set.reshape(1,-1)) ## One line of values, normalized.  These are the test values
+# predictions = model.predict(X_test_scaled) ## Output - what should these values give you?
 
-print("Predicted: {}, actual: {}".format(predictions[0], 'M'));
+# print("Predicted: {}, actual: {}".format(predictions[0], 'M'));
--- test_cancer.py
+++ test_cancer.py
@@ -3,40 +3,57 @@ from BreastCancerML import *
 
 class TestBreastCancerPredictionMachine(unittest.TestCase):
 
-    # The input array needs 31 elements
-    test_set = np.array([0.1425, 0.2839, 0.2414, 0.1052, 0.2597, 0.09744, 0.4956, 0.00911, 0.07458, 0.05661, 0.01867, 0.05963, 0.009208, 0.2098, 0.8663, 0.6869, 0.2575, 0.6638, 0.173, 1.156, 3.445, 11.42, 14.91, 20.38, 26.5, 27.23, 77.58, 98.87, 386.1, 567.7, 84348301])
-
-    def test_read_csv(self):
-        data = pd.read_csv('Data/data.csv', index_col=False)
-        self.assertFalse(data.empty)
-
-    def test_pumper_NoneInput(self):
-        pumper = BreastCancerPredictionMachine()
-        input_data = None
-        result = pumper.getData(input_data)
-        self.assertEqual(result, 0)
-
-    def test_pumper_SomeInput(self):
-        pumper = BreastCancerPredictionMachine()
-        input_data = 'data.csv'
-        result = pumper.getData(input_data)
-        self.assertEqual(result, 1)
-
-    def test_pumper_NotCsv(self):
-        pumper = BreastCancerPredictionMachine()
-        input_data = 'data'
-        result = pumper.getData(input_data)
-        self.assertEqual(result, 0)
-
     def test_setInput_Exists(self):
         machine = BreastCancerPredictionMachine()
-        result = machine.setInput(test_set)
+        test_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+        result = machine.setInput(test_data)
         self.assertIsNotNone(result)
 
-    def test_setInput_Returns_1_On_Success(self):
+    def test_setInuput_Does_Not_Take_Less_Than_31_Elements(self):
         machine = BreastCancerPredictionMachine()
-        result = machine.setInput(test_set)
-        self.assertEqual(result, 1)
+        test_data = [1, 2, 3]
+        with self.assertRaises(ValueError):
+          machine.setInput(test_data)
+
+    def test_setInuput_Does_Not_Take_More_Than_31_Elements(self):
+        machine = BreastCancerPredictionMachine()
+        test_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+        with self.assertRaises(ValueError):
+          machine.setInput(test_data)
+
+    def test_setInput_Returns_Numpy_Array(self):
+        machine = BreastCancerPredictionMachine()
+        test_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+        result = machine.setInput(test_data)
+        self.assertEqual(result.size, 31)
+
+    def test_getDiagnosis_Returns_Single_Character_On_Success(self):
+        machine = BreastCancerPredictionMachine()
+        test_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+        result = machine.getDiagnosis(test_data)
+        self.assertEqual(len(result), 1)
+
+    def test_getDiagnosis_Raises_TypeError_Exception_If_Input_Is_Not_List(self):
+        machine = BreastCancerPredictionMachine()
+        test_data = "not a list"
+        with self.assertRaises(TypeError):
+            machine.getDiagnosis(test_data)
+
+    def test_getDiagnosis_Predicts_Malignant_Correctly(self):
+        machine = BreastCancerPredictionMachine()
+        test_data = [0.1425, 0.2839, 0.2414, 0.1052, 0.2597, 0.09744, 0.4956, 0.00911, 0.07458, 0.05661, 0.01867, 0.05963, 0.009208, 0.2098, 0.8663, 0.6869, 0.2575, 0.6638, 0.173, 1.156, 3.445, 11.42, 14.91, 20.38, 26.5, 27.23, 77.58, 98.87, 386.1, 567.7, 84348301]
+        result = machine.getDiagnosis(test_data)
+        self.assertEqual(result, 'M')
+    
+    def test_getDiagnosis_Predicts_Benign_Correctly(self):
+        machine = BreastCancerPredictionMachine()
+        test_data = [8510426,13.54,14.36,87.46,566.3,0.09779,0.08129,0.06664,0.04781,0.1885,0.05766,0.2699,0.7886,2.058,23.56,0.008462,0.0146,0.02387,0.01315,0.0198,0.0023,15.11,19.26,99.7,711.2,0.144,0.1773,0.239,0.1288,0.2977,0.07259]
+        result = machine.getDiagnosis(test_data)
+        self.assertEqual(result, 'B')
+    
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
